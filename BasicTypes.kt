0bda89d7e8ff1c28650f3a74e2848c20c78f04d0
BraavoSEP <jonasdecarli@gmail.com>
2025-02-12 11:08:33 -0300

    Add script de tipos básicos

---
+++ BasicTypes.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val inteiro: Int = 10
+    val double: Double = 3.14
+    val booleano: Boolean = true
+    val char: Char = 'A'
+    val string: String = "Olá, Kotlin!"
+
+    println("Inteiro: $inteiro, Double: $double, Boolean: $booleano, Char: $char, String: $string")
+}
